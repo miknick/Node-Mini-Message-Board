588c860e846bdb641b298787d29804c0b16a7aca
Yunus Akten <yunusakten2@gmail.com>
2021-03-28 20:28:31 +0300

    first and last commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+router.post("/new", function (req, res, next) {
+  messages.push({ text: req.body.message, user: req.body.user, added: new Date() });
+  res.redirect("/")
+})
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: ' Mini Message Board', messages: messages });
+});
+
+
+module.exports = router;
